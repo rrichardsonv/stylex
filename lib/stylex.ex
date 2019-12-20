f1f6e16a74cda10180e5884e04af79573c6f2a78
Rich Richardson <rrichardsonv+github@gmail.com>
2019-12-20 10:16:18 -0600

    initial commit

---
+++ lib/stylex.ex
@@ -0,0 +1,18 @@
+defmodule Stylex do
+  @moduledoc """
+  Documentation for Stylex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Stylex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/stylex_test.exs
@@ -0,0 +1,8 @@
+defmodule StylexTest do
+  use ExUnit.Case
+  doctest Stylex
+
+  test "greets the world" do
+    assert Stylex.hello() == :world
+  end
+end
